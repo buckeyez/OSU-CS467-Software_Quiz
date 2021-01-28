aae7e64c26580c3ef82b7da553b178aacdb9e651
buckeyez <masonx88@gmail.com>
2021-01-27 21:50:28 -0600

    add more fields to user model

--- Models/AppUser.cs
+++ Models/AppUser.cs
@@ -1,8 +1,14 @@
 using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace OSU_CS467_Software_Quiz.Models
 {
   public class AppUser : IdentityUser
   {
+    [ProtectedPersonalData]
+    public string FirstName { get; set; }
+    
+    [ProtectedPersonalData]
+    public string LastName { get; set; }
   }
 }
