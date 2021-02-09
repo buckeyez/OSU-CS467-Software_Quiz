73012bd3c15c6dbf164b90df504c4249a8fbbf72
Miao <miao@lumen.lan>
2021-02-08 22:48:21 -0800

    now can display pool, and dynamically display entire question of free response and true or false

--- ClientApp/src/components/Questions/TrueOrFalseQuestion.js
+++ ClientApp/src/components/Questions/TrueOrFalseQuestion.js
@@ -1,14 +1,26 @@
 
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { TransitionPropTypeKeys } from 'reactstrap/lib/utils';
 
 
 
 const TrueOrFalseQuestion = (props) =>{
+
+  const [answer, setAnswer] = useState(props.answers[0].value)
+  const handleUpdate = (e) => {
+    if(answer === "true"){
+      setAnswer("false")
+    }else{
+      setAnswer("true")
+    }
+    props.handleTrueOrFalseAnswer(e);
+
+  }
   return (
-    <div onChange={props.handleTrueOrFalseAnswer}>
-      <input type="radio" name="trueOrFalse" value="true" /> True
-      <input type="radio" name="trueOrFalse" value="false" />False
+    <div onChange={(e) => handleUpdate(e)}>
+      {console.log("answer is ", props.answers[0].value, answer)}
+      <input type="radio" name="trueOrFalse" value="true" checked={answer==="true"}/> True
+      <input type="radio" name="trueOrFalse" value="false"checked={answer==="false"}/>False
     </div>
   )
 }
