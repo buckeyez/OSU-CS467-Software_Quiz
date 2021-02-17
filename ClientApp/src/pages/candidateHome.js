b0248d670422f252ab904066b7d0fb60d8f782e1
Chaudhra <47132229+Chaudhra@users.noreply.github.com>
2021-02-17 14:17:20 -0500

    Implemented QuizCard component

--- ClientApp/src/pages/candidateHome.js
+++ ClientApp/src/pages/candidateHome.js
@@ -2,17 +2,27 @@ import React, { useContext } from 'react';
 import { UserContext } from '../context/userContext';
 import { useHistory } from 'react-router-dom';
 import * as ROUTES from '../constants/routes';
-import { Welcome } from '../components';
+import { Welcome, QuizCard } from '../components';
 
 export default function CandidateHome() {
   //   const { user } = useContext(UserContext);
   const history = useHistory();
+
+  /*TODO: Refactor this code so that we are dynamically obtaining the number of
+   * of quizes assigned to the user. Right now its hard coded. You'd ideally
+   * want to map over the number of quizes on the user object, and then render
+   * each QuizCard component.
+   */
 
   return (
     <>
       <Welcome>
         <Welcome.Title>Welcome [NAME HERE] </Welcome.Title>
         <Welcome.TextSmall>You have been assigned the following quiz(s). </Welcome.TextSmall>
+        <QuizCard>
+          <QuizCard.Title>Your awesome quiz</QuizCard.Title>
+          <QuizCard.ButtonLink to={ROUTES}>Start Quiz</QuizCard.ButtonLink>
+        </QuizCard>
       </Welcome>
     </>
   );
