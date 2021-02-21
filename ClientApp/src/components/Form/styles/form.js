3681b4addca99469179d5b0e8b4919e91b1fd9d7
Miao <miao@lumen.lan>
2021-02-20 18:34:11 -0800

    Added basic style

--- ClientApp/src/components/Form/styles/form.js
+++ ClientApp/src/components/Form/styles/form.js
@@ -15,3 +15,24 @@ export const TextSmall = styled.p``;
 export const Link = styled.link``;// styled(ReactRouterLink)``;
 export const Input = styled.input``;
 export const Submit = styled.button``;
+
+export const Quizzes = styled.div`
+        border: solid 1px black;
+        border-radius: 10px;
+        padding: 20px;
+        margin: 10px;
+        display: inline-block;
+    `;
+
+export const QuestionsNextToQuizzes = styled.div`
+        border: solid 1px black;
+        width: 100%;
+        border-radius: 10px;
+        padding: 20px;
+        margin: 10px;
+    `;
+
+export const QuizOuter = styled.div`
+        // border: solid 1px black;
+        display: flex;
+    `;
